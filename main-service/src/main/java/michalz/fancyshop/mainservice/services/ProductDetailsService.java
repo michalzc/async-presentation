9187baf9e5841fbde67b84a224635fa284e7ebd2
Michał Zając <michal.t.zajac@gmail.com>
2015-05-09 22:33:48 +0200

    Asynchronous servlet.

--- main-service/src/main/java/michalz/fancyshop/mainservice/services/ProductDetailsService.java
+++ main-service/src/main/java/michalz/fancyshop/mainservice/services/ProductDetailsService.java
@@ -1,11 +1,14 @@
 package michalz.fancyshop.mainservice.services;
 
+import lombok.extern.slf4j.Slf4j;
 import michalz.fancyshop.dto.ProductDetails;
 import michalz.fancyshop.dto.ProductInfo;
 import michalz.fancyshop.dto.ProductReviews;
 import michalz.fancyshop.dto.ProductSuggestions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -13,6 +16,7 @@ import java.util.concurrent.Future;
 /**
  * Created by michal on 09.05.15.
  */
+@Slf4j
 public class ProductDetailsService {
 
     @Autowired
@@ -40,6 +44,14 @@ public class ProductDetailsService {
         productDetails.setProductSuggestions(productSuggestionsFuture.get());
 
         return productDetails;
+    }
+
+    @Async
+    public void productDetails(Long productId, DeferredResult<ProductDetails> result) throws ExecutionException, InterruptedException {
+        log.info("Getting product details for {}", productId);
+        ProductDetails productDetails = getProductDetails(productId);
+        result.setResult(productDetails);
+        log.info("Result updated");
     }
 
 }
--- main-service/src/main/java/michalz/fancyshop/mainservice/web/ApiController.java
+++ main-service/src/main/java/michalz/fancyshop/mainservice/web/ApiController.java
@@ -1,9 +1,11 @@
 package michalz.fancyshop.mainservice.web;
 
+import lombok.extern.slf4j.Slf4j;
 import michalz.fancyshop.dto.ProductDetails;
 import michalz.fancyshop.mainservice.services.ProductDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.ExecutionException;
 
@@ -12,13 +14,17 @@ import java.util.concurrent.ExecutionException;
  */
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class ApiController {
 
     @Autowired
     private ProductDetailsService productDetailsService;
 
     @RequestMapping(value = "/productdetails/{productId}", method = RequestMethod.GET)
-    public ProductDetails getProductDetails(@PathVariable("productId")Long productId) throws ExecutionException, InterruptedException {
-        return productDetailsService.getProductDetails(productId);
+    public DeferredResult<ProductDetails> getProductDetails(@PathVariable("productId")Long productId) throws ExecutionException, InterruptedException {
+        log.info("Handling productdetails request for {}", productId);
+        DeferredResult<ProductDetails> result = new DeferredResult<>();
+        productDetailsService.productDetails(productId, result);
+        return result;
     }
 }
